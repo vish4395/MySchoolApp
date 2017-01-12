8c6ef9bac2250e220a632d89581b3b632291d78b
vish4395 <newaivirus@gmail.com>
2017-01-12 22:57:02 +0530

    myscool

---
+++ platforms/browser/www/js/api_conf - Copy.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var apiUrl = "http://newai.in/msa1/APP/";
+//var apiUrl = "app/index.php/";
+
+var uploadpath = "http://newai.in/msa1/APP/upload/";
+//var uploadpath="app/upload";
+
+var document_path = "newai.in/msa1/APP/upload/documents/";
+//var document_path="app/upload/documents/";
---
+++ www/js/api_conf.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var apiUrl = "http://localhost/vs/msa/new1/APP/";
+//var apiUrl = "app/index.php/";
+
+var uploadpath = "http://localhost/vs/msa/new1/APP/upload/";
+//var uploadpath="app/upload";
+
+var document_path = "http://localhost/vs/msa/new1/APP/upload/documents/";
+//var document_path="app/upload/documents/";
